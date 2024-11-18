ae12f94f06fd38fb05279f1d831a9a4e66a42c4c
Brigham Pettit <bpettit2003@gmail.com>
2024-11-18 14:47:44 -0600

    Add Jest for testing client

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,9 @@
+import React from 'react'
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders todo list', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByText(/to-do list/i);
+  expect(headingElement).toBeInTheDocument();
 });
